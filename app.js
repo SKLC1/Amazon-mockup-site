a54bc2ab023e79b95b1f4f7cff54b200525ccae2
SKLC1 <segevklein2000@gmail.com>
2022-04-10 02:18:28 +0300

    js

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let img = document.querySelector('#img1');
+let btn1 = document.querySelector('#link1');
+let btn2 = document.querySelector('#link2');
+let btn3 = document.querySelector('#link3');
+let btn4 = document.querySelector('#link4');
+let btn5 = document.querySelector('#link5');
+let btn6 = document.querySelector('#link5');
+
+link1.addEventListener('click',() =>{
+  img1.src='https://m.media-amazon.com/images/I/71jqfwxHzVL._AC_SX569_.jpg';
+})
+
+link2.addEventListener('click',() =>{
+  img1.src='https://m.media-amazon.com/images/I/716Jetv1OOL._AC_SY450_.jpg';
+})
+link3.addEventListener('click',() =>{
+  img1.src='https://m.media-amazon.com/images/I/71P44V6UE3L._AC_SY450_.jpg';
+})
+link4.addEventListener('click',() =>{
+  img1.src='https://m.media-amazon.com/images/I/71HTwx7Ul7L._AC_SY450_.jpg';
+})
+link5.addEventListener('click',() =>{
+  img1.src='https://m.media-amazon.com/images/I/71H77gr1zwL._AC_SY450_.jpg';
+})
+link6.addEventListener('click',() =>{
+  img1.src='';
+})
